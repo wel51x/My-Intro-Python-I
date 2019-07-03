393b32e03387d4810c533c4c1c9c79b3341f398c
Winston <wxl51@yahoo.com>
2019-07-02 21:59:18 -0700

    Modify 15_classes.py code

--- src/15_classes.py
+++ src/15_classes.py
@@ -26,12 +26,12 @@ class Waypoint(LatLon):
     name = ""
 
     def __init__(self, la, lo, name):
-        LatLon.__init__(self, la, lo)
+        super().__init__(la, lo)
         self.name = name
     
     def display(self): 
         print("Name = " + str(self.name)) 
-        LatLon.display(self)
+        super().display()
 
     def __str__(self):
         return("Name: " + self.name + ", Lat: " + str(self.lat) + ", Long: " + str(self.lon))
@@ -43,6 +43,23 @@ wp = Waypoint(41.999, -121.999, "somewhere")
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
+class Geocache(Waypoint): 
+    difficulty = 0.0
+    size = 0
+
+    def __init__(self, la, lo, name, difficulty, size):
+        super().__init__(la, lo, name)
+        self.difficulty = difficulty
+        self.size = size
+    
+    def display(self): 
+        super().display()
+        print("Difficulty = " + str(self.difficulty)) 
+        print("Size = " + str(self.size))
+
+    def __str__(self):
+        return("Name: " + self.name + ", Lat: " + str(self.lat) + ", Long: " + str(self.lon) +
+         ", Difficulty: " + str(self.difficulty) + ", Size: " + str(self.size))
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
@@ -56,7 +73,8 @@ print(waypoint)
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
+geocache = Geocache(44.052137, -121.41556, "Newberry Views", 1.5, 2)
+#gc.display()
 
 # Print it--also make this print more nicely
-# didn't feel like doing it right now
-# print(geocache)
+print(geocache)
